d5127b3555a9294d689fcd5aae2d4053adc526e4
ZerickSyahputra <zericksyahputra@students.amikom.ac.id>
2024-03-19 09:40:59 +0700

    first commit

---
+++ ClassAndObject/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat object Taxi
+            taxi taxi = new taxi();
+
+            // membuat nilai properti
+            taxi.DriverName = "Zerick";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PicUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
